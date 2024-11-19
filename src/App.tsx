f71944e93fb7dd96817779b4cbf87a9c6be1f720
Razzoy <1081892@ucn.dk>
2024-11-19 12:17:53 +0100

    Building skeleton v2

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Home } from "./pages/Home"
+import { MainLayout } from "./layouts/MainLayout"
+import { MyRecipes } from "./pages/MyRecipes"
+import { CreateRecipe } from "./pages/CreateRecipe"
+import { Recipe } from "./pages/Recipe"
+import { Grocery } from "./pages/Grocery"
+import { Login } from "./pages/Login"
+import { NoPage } from "./pages/NoPage"
 
 function App() {
 
   return (
+      <>
       <BrowserRouter>
         <Routes>
           <Route path={'/'} element={<MainLayout />}>
             <Route index element={<Home />} />
             <Route path='myRecipes' element={<MyRecipes />} />
             <Route path='createRecipe' element={<CreateRecipe />} />
-            <Route path='groceryList' element={<GroceryList />} />
+            <Route path='grocery' element={<Grocery />} />
             <Route path='recipe' element={<Recipe />} />
             <Route path='login' element={<Login />} />
             <Route path={'/*'} element={<NoPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
+      </>
 
   )
 }
---
+++ src/pages/MyRecipes.tsx
@@ -0,0 +1,5 @@
+export function MyRecipes() {
+  return (
+    <div>MyRecipes</div>
+  )
+}
