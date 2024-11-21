edc785064efb10f4d9cb8ad5a92ebcad38621f23
Razzoy <1081892@ucn.dk>
2024-11-21 15:08:42 +0100

    Version 2
    Should be final

--- src/components/NavBar/Navbar.tsx
+++ src/components/NavBar/Navbar.tsx
@@ -1,20 +1,29 @@
 import { NavLink } from "react-router-dom";
 import { NavBarIcon } from "./NavBarIcon/NavBarIcon";
+import { useState } from "react";
+
 
 export function Navbar() {
+
+  const [activeIcon, setActiveIcon] = useState("Home");
+
+  const toggleActive = (icon) => {
+    setActiveIcon(icon);
+  }
+
   return (
     <div className="flex justify-between w-full p-3 px-6 fixed bottom-0">
-      <NavLink className={'flex group items-center'} to="/">
-        <NavBarIcon name="Home"/>
+      <NavLink onClick={()=> toggleActive ("Home")} className={'flex group items-center '} to="/">
+        <NavBarIcon name="Home" fill={activeIcon === "Home" ? "#ff7463" : "Black"} stroke=""/>
       </NavLink>
-      <NavLink className={'flex group items-center'} to="/myRecipes">
-        <NavBarIcon name="Search"/>
+      <NavLink onClick={()=> toggleActive ("Search")} className={'flex group items-center'} to="/myRecipes">
+        <NavBarIcon name="Search" fill={activeIcon === "Search" ? "#ff7463" : "Black"} stroke=""/>
       </NavLink>
-      <NavLink className={'flex group items-center'} to="/createRecipe">
-      <NavBarIcon name="Add" />
+      <NavLink onClick={()=> toggleActive ("Add")} className={'flex group items-center'} to="/createRecipe">
+      <NavBarIcon name="Add" fill="" stroke={activeIcon === "Add" ? "#ff7463" : "Black"} />
       </NavLink>
-      <NavLink className={'flex group items-center'} to="/grocery">
-      <NavBarIcon name="List" />
+      <NavLink onClick={()=> toggleActive ("List")} className={'flex group items-center'} to="/grocery">
+      <NavBarIcon name="List" fill={activeIcon === "List" ? "#ff7463" : "Black"} stroke=""/>
       </NavLink>
     </div>
   )
