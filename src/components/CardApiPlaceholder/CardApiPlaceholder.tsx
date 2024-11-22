917bf0478ba692c4ced1c1d638f493f97d8baba4
Razzoy <1081892@ucn.dk>
2024-11-22 15:05:27 +0100

    1st itteration supabase

---
+++ src/components/CardApiPlaceholder/CardApiPlaceholder.tsx
@@ -0,0 +1,45 @@
+import { useEffect, useState } from 'react';
+import { supabase } from '../../lib/supabaseClient';
+
+type Recipe = {
+    id: number;
+    title: string;
+    time: number;
+    difficulty: string;
+    ingredients: string[];
+    instructions: string;
+    image: string;
+    user_id: number;
+  };
+
+export function CardApiPlaceholder() {
+
+    const [recipes, setRecipes] = useState<Recipe[]>([]);
+
+    useEffect(() => {
+        const getRecipes = async () => {
+            const { data, error } = await supabase.from("Recipes").select();
+            console.log(data);
+            
+            if (error) {
+                console.error(`Error fetching recipes: ${error.message}`);
+            } else {
+                if (data && Array.isArray(data)) {
+                    setRecipes(data);
+                } else {
+                    console.warn("Unexpected data format:", data);
+                }
+            }
+        };
+
+        getRecipes();
+    }, []);
+
+    useEffect(() => {
+        // console.log(recipes);
+    }, [recipes]);
+
+    return (
+        <div>CardApiPlaceholder</div>
+    );
+}
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from "react-router-dom";
 import { Navbar } from "../components/NavBar/Navbar";
 import { Header } from "../components/Header/Header";
+import { CardApiPlaceholder } from "../components/CardApiPlaceholder/CardApiPlaceholder";
 
 
 export function MainLayout() {
   return (
     <>
     <Header/>
+    <CardApiPlaceholder/>
     <main>
         <Outlet/>
     </main>
