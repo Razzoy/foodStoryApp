ca4f87119cfdbe536fddafcddeb76fd1736bb1a4
Kristin <kristineva9@gmail.com>
2024-11-25 11:37:35 +0000

    issue#7 made a home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,29 @@
-import { SearchBar } from "../components/SearchBar/SearchBar";
+import { Card } from "../components/Card/Card";
 
 export function Home() {
   return (
-    <SearchBar/>
-  )
+    <div>
+      <h1 className="font-Nunito text-xl p-4">My recipes</h1>
+      <div className="flex space-x-7 p-4 overflow-hidde w-max">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+      <h1 className="font-Nunito text-xl p-4">Favorites</h1>
+      <div className="flex overflow-hidden w-max space-x-7 p-4 ">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+      <h1 className="font-Nunito text-xl p-4">Recommendation</h1>
+      <div className="flex overflow-hidden w-max space-x-7 p-4">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  );
 }
