c6be0c474a298d430a74b58b4bfbc2a2e5db061c
magneamist <magneamist@gmail.com>
2024-11-26 17:51:30 +0000

    made the card more dinamyc and added the heart function

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,17 +1,52 @@
-export function Card() {
+import { useState } from "react";
+
+export type CardProps = {
+  title?: string;
+  time?: string;
+  image?: string;
+};
+
+export function Card({ title, time, image }: CardProps) {
+  const [isLiked, setIsLiked] = useState(false);
+
+  //url('/src/assets/img/Cheeseburgers.png')
+
   return (
-    <div className="bg-[url('/src/assets/img/Cheeseburgers.png')] bg-center bg-cover rounded-xl w-44 h-64 flex flex-col justify-end">
+    <div
+      className="bg-center bg-cover rounded-xl w-44 h-64 flex flex-col justify-end"
+      style={{
+        backgroundImage: `url(${image ?? "/src/assets/img/Cheeseburgers.png"})`,
+      }}
+    >
       <div className="bg-gradient-to-t from-lime-800 from 100% rounded-xl h-36 flex flex-col justify-end">
         <div className="p-2 flex flex-col gap-2.5">
           <h3 className="text-white self-stretch text-sm font-semibold">
-            Hamburgers
+            {title ?? "Cheeseburgers"}
           </h3>
           <div className="flex justify-between pr-4">
             <div className="flex items-center gap-1">
               <img src="/src/assets/icons/Watch.svg" alt="" className="w-3" />
-              <p className="text-white text-xs">30 min</p>
+              <p className="text-white text-xs">{time ?? "30 min"}</p>
             </div>
-            <img src="/src/assets/icons/HeartEmpty.svg" alt="" />
+            <button onClick={() => setIsLiked(!isLiked)}>
+              {" "}
+              <svg
+                width="18"
+                height="16"
+                viewBox="0 0 18 16"
+                fill={isLiked ? "#FD7463" : "none"}
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  id="Vector"
+                  d="M13.1667 1C10.5 1 9 3.33363 9 4.5C9 3.33363 7.5 1 4.83333 1C2.16667 1 1.5 3.33363 1.5 4.5C1.5 10.625 9 15 9 15C9 15 16.5 10.625 16.5 4.5C16.5 3.33363 15.8333 1 13.1667 1Z"
+                  stroke="#FD7463"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            </button>
           </div>
         </div>
       </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,30 +4,30 @@ export function Home() {
   return (
     <div className="pt-24">
       <h1 className="font-Nunito text-xl p-4">My recipes</h1>
-      <div className="flex p-4 overflow-scroll w-full"> 
+      <div className="flex p-4 overflow-scroll w-full">
         <div className="flex space-x-7">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+          <Card title="lasagna" time="1 klst" />
+          <Card />
+          <Card />
+          <Card />
         </div>
       </div>
       <h1 className="font-Nunito text-xl p-4">Favorites</h1>
       <div className="flex p-4 overflow-scroll w-full">
         <div className="flex space-x-7">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
         </div>
       </div>
       <h1 className="font-Nunito text-xl p-4">Recommendation</h1>
       <div className="flex p-4 overflow-scroll w-full">
         <div className="flex space-x-7">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
         </div>
       </div>
     </div>
