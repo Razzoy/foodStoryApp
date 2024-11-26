9f8755fef014dbeea1d734406878bbedba6e10e8
Kristín Eva Ólafsdóttir <kristineva9@gmail.com>
2024-11-26 10:59:17 +0000

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,29 @@
+import { Card } from "../components/Card/Card";
+
 export function Home() {
   return (
-   <>
-   </>
-  )
+    <div>
+      <h1 className="font-Nunito text-xl p-4">My recipes</h1>
+      <div className="flex space-x-7 p-4 overflow-hidde w-max">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+      <h1 className="font-Nunito text-xl p-4">Favorites</h1>
+      <div className="flex overflow-hidden w-max space-x-7 p-4 ">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+      <h1 className="font-Nunito text-xl p-4">Recommendation</h1>
+      <div className="flex overflow-hidden w-max space-x-7 p-4">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  );
 }
