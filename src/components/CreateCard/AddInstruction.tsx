4da1ce7a57128f1cfa57a91a8f95902020e514b0
magneamist <magneamist@gmail.com>
2024-11-27 12:30:11 +0000

    components to create recipe

--- src/components/CreateCard/AddInstruction.tsx
+++ src/components/CreateCard/AddInstruction.tsx
@@ -1,12 +1,43 @@
-export function AddInstruction() {
+import { SetStateAction, useState } from "react";
+
+export function AddInstruction({
+  onAdd,
+  currentInstructionList,
+}: {
+  onAdd: React.Dispatch<
+    React.SetStateAction<
+      {
+        text: string;
+      }[]
+    >
+  >;
+  currentInstructionList: {
+    text: string;
+  }[];
+}) {
+  const [text, setText] = useState("");
+
   return (
-    <div className="flex gap-2 self-stretch text-sm">
-      <p>1.</p>
-      <input
-        type="text"
-        placeholder="Write the 1. step of the process..."
-        className="border-b-2 w-full outline-none focus:border-black"
-      />
+    <div className="flex flex-col gap-2 self-stretch text-sm">
+      <div className="flex gap-2">
+        <p>{currentInstructionList.length + 1}.</p>
+        <input
+          type="text"
+          placeholder="Write the 1. step of the process..."
+          className="border-b-2 w-full outline-none focus:border-black"
+          onChange={(e) => setText(e.target.value) }
+          value={text}
+          id=""
+          name=""
+        />
+      </div>
+      <button
+        onClick={() => onAdd([...currentInstructionList, { text }])}
+        className="border-2 border-salmon rounded-xl py-1 px-1.5 self-end"
+      >
+        {" "}
+        <img src="/src/assets/icons/Plus.svg" alt="" />
+      </button>
     </div>
   );
 }
--- src/components/CreateCard/ScratchCard/ScratchCard.tsx
+++ src/components/CreateCard/ScratchCard/ScratchCard.tsx
@@ -32,72 +32,70 @@ export function ScratchCard() {
     }[]
   >([]);
 
-  // const Ingredient = () => { return <
+  const [instructionList, setInstructionList] = useState<
+    {
+      text: string;
+    }[]
+  >([]);
 
-  //   const Form = () => {
-  //     const [inputList, setInputList] = useState([]);
-
-  //     const onAddBtnClick = event => {
-  //     setInputList(inputList.concat(<Ingredient />));
-  //   };
-
-  function AddIngredientComp() {}
   return (
     <div className="flex flex-col gap-5 items-start self-stretch mt-2">
       <div className="flex flex-col self-start gap-1">
         <p className="text-sm">How many servings?</p>
         <div className="flex self-start gap-2 items-center">
-          <svg
-            width="18"
-            height="18"
-            viewBox="0 0 14 14"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            onClick={decrement}
-          >
-            <g id="minus-sign-stroke-rounded">
-              <path
-                id="Vector"
-                d="M11.6667 7H2.33337"
-                stroke="black"
-                stroke-width="1.5"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </g>
-          </svg>
+          <button onClick={decrement}>
+            <svg
+              width="18"
+              height="18"
+              viewBox="0 0 14 14"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g id="minus-sign-stroke-rounded">
+                <path
+                  id="Vector"
+                  d="M11.6667 7H2.33337"
+                  stroke="black"
+                  stroke-width="1.5"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </g>
+            </svg>
+          </button>
           <label htmlFor="" className="self-center text-xl">
             {count}
           </label>
-          <svg
-            width="20"
-            height="20"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-            onClick={increment}
-          >
-            <g id="plus-sign-stroke-rounded">
-              <path
-                id="Vector"
-                d="M12 4V20M20 12H4"
-                stroke="black"
-                stroke-width="2"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-              />
-            </g>
-          </svg>
+          <button onClick={increment}>
+            <svg
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <g id="plus-sign-stroke-rounded">
+                <path
+                  id="Vector"
+                  d="M12 4V20M20 12H4"
+                  stroke="black"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                />
+              </g>
+            </svg>
+          </button>
         </div>
       </div>
       <div>
-        <h2 className="text-xl">Ingredients</h2>
+        <h2 className="text-base">Ingredients</h2>
         <div className="flex flex-col items-start gap-3 self-stretch">
           {ingredientList.map((ingredient, index) => (
-            <div className="flex gap-4">
-              <p key={index}>{ingredient.name}</p>
+            <div className="flex gap-1">
               <p key={index}>{ingredient.quantity}</p>
               <p key={index}>{ingredient.unit}</p>
+              <p key={index}>{ingredient.name}</p>
             </div>
           ))}
           <AddIngredient
@@ -106,13 +104,18 @@ export function ScratchCard() {
           />
         </div>
       </div>
+      <h2 className="text-base">Instructions</h2>
       <div className="flex flex-col items-start gap-3 self-stretch">
-        <h2 className="text-xl">Instructions</h2>
-        <AddInstruction />
-        <button className="border-2 border-salmon rounded-xl py-1 px-1.5 self-end">
-          {" "}
-          <img src="/src/assets/icons/Plus.svg" alt="" />
-        </button>
+        {instructionList.map((instruction, index) => (
+          <div className="flex gap-4">
+            <p key={index}>{index + 1}.</p>
+            <p key={index}>{instruction.text}</p>
+          </div>
+        ))}
+        <AddInstruction
+          onAdd={setInstructionList}
+          currentInstructionList={instructionList}
+        />
       </div>
     </div>
   );
