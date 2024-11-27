e3ca9d500cb24c9eb34e260072d17679fe3ddeb9
Angelika Ciuchcińska <angelikaciuchcinska@Angelikas-MacBook-Air.local>
2024-11-27 12:54:36 +0000

    I´m trying to create sceloton, but I´m not finish

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,39 +5,39 @@ export function Home() {
 
     <div className="">
       <div className="p-4">
-      <h1 className="font-Nunito text-xl pb-2">My recipes</h1>
-      <div className="flex overflow-scroll w-full"> 
-        <div className="flex space-x-7">
-          <Card title="lasagna" time="1 klst" />
-          <Card />
-          <Card />
-          <Card />
+        <h1 className="font-Nunito text-xl pb-2">My recipes</h1>
+        <div className="flex overflow-scroll w-full">
+          <div className="flex space-x-7">
+            <Card title="lasagna" time="1 klst" />
+            <Card />
+            <Card />
+            <Card />
+          </div>
         </div>
       </div>
-      </div>
       <div className="p-4">
-      <h1 className="font-Nunito text-xl pb-2 ">Favorites</h1>
-      <div className="flex  overflow-scroll w-full">
-        <div className="flex space-x-7">
+        <h1 className="font-Nunito text-xl pb-2 ">Favorites</h1>
+        <div className="flex  overflow-scroll w-full">
+          <div className="flex space-x-7">
 
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-              <button>See more</button>
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <button>See more</button>
+          </div>
         </div>
-      </div>
       </div>
       <div className="p-4">
-      <h1 className="font-Nunito text-xl pb-2">Recommendation</h1>
-      <div className="flex overflow-scroll w-full">
-        <div className="flex space-x-7">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+        <h1 className="font-Nunito text-xl pb-2">Recommendation</h1>
+        <div className="flex overflow-scroll w-full">
+          <div className="flex space-x-7">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+          </div>
         </div>
-      </div>
       </div>
     </div>
   );
