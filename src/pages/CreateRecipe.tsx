1ade4d8c4b426e77c3bc311e3e2aece9db1da89f
Razzoy <61683221+Razzoy@users.noreply.github.com>
2024-11-28 10:57:31 +0100

    Supabase data to card

--- src/pages/CreateRecipe.tsx
+++ src/pages/CreateRecipe.tsx
@@ -1,14 +1,8 @@
 
-import { CardApiPlaceholder } from "../components/CardApiPlaceholder/CardApiPlaceholder";
-import UploadRecipe from "../components/CardApiPlaceholder/UploadRecipe/UploadRecipe";
-
-
 export function CreateRecipe() {
 
   return (
     <div>
-          <CardApiPlaceholder/>
-          <UploadRecipe/>
     </div>
   )
 
