3f67997b2b61c80836b0c39639ce45dd6f0f82ec
magneamist <magneamist@gmail.com>
2024-12-01 19:40:13 +0000

    icons on modal were not showing so i fixed it

--- src/components/BurgerModal/BurgerModal.tsx
+++ src/components/BurgerModal/BurgerModal.tsx
@@ -12,15 +12,15 @@ export function BurgerModal({ hidemodal }: Props) {
       <div className="flex flex-col gap-6 h-full">
         <div className="text-white flex flex-col justify-center gap-2 h-full items-center">
           <div className="flex gap-3">
-            <img className="w-4" src="src/assets/icons/Settings.svg"></img>
+            <img className="w-4" src="/src/assets/icons/Settings.svg"></img>
             <button className="text-left w-48">Account settings</button>
           </div>
           <div className="flex gap-3">
-            <img className="w-3" src="src/assets/icons/Help.svg"></img>
+            <img className="w-3" src="/src/assets/icons/Help.svg"></img>
             <button className="text-left w-48">Help center</button>
           </div>
           <div className="flex gap-3">
-            <img className="w-4" src="src/assets/icons/Terms.svg"></img>
+            <img className="w-4" src="/src/assets/icons/Terms.svg"></img>
             <button className="text-left w-48">Terms and conditions</button>
           </div>
 
